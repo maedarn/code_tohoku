cc4e66ca92aabc263f9473f8a8e1a815f83161bc
r-maeda <maeda.ryunosuke@j.mbox.nagoya-u.ac.jp>
2018-05-09 01:32:59 +0900

    波数（圧力）を追加

--- Bfield/Bfield.f90
+++ Bfield/Bfield.f90
@@ -1,13 +1,12 @@
 program main
   implicit none
-  integer i,n,j
-  double precision B0,B1,x,y,dx,dy,Bx,By,dBx,dBy,a,k,omega,t
-  !CHARACTER(33) :: dir='/Users/maeda/Desktop/code/Bfield/'
-  !character(4) :: filenm='BF01'
+  integer i,n,j,l
+  double precision B0,B1,x,y,dx,dy,Bx,By,dBx,dBy,a,k,omega,t,pi,xpiku,ypiku
+  CHARACTER(33) :: dir='/Users/maeda/Desktop/code/Bfield/'
+  character(4) :: filenm='BF01'
   character filename*128
 100 format(E19.10e3,E19.10e3)
-  ! open(10,file=dir//filenm//'.dat')
-
+  open(10,file=dir//filenm//'.dat')
 
   x=0.0d0
   y=0.0d0
@@ -19,6 +18,16 @@ program main
   B1=3.0d0
   dx=1.0d0/100.0d0
   dy=1.0d0/100.0d0
+  xpiku=0.0d0
+  ypiku=0.0d0
+  pi=3.14d0
+
+  do l=1,5
+     xpiku=dsqrt(1.0d0*pi/k)+xpiku
+     ypiku=dsqrt(1.0d0*pi/k)+ypiku
+     write(10,100) xpiku,ypiku
+     !write(10,*)
+  end do
 
   do j= 1,5
      t=0.2*int(j)
